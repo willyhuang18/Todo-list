c74c47c66b81a8277d6162eb1655f4d52f5f0c59
wiily <willyhuang18@gmail.com>
2022-03-23 21:23:02 -0700

    making a delete route for user to delete the item they don't want

--- app.js
+++ app.js
@@ -106,3 +106,25 @@ app.post("/", function(req, res){
       });
     }
 });
+
+app.post("/delete", function(req, res){
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+  
+    if (listName === "Today") {
+      Item.findByIdAndRemove(checkedItemId, function(err){
+        if (!err) {
+          console.log("Successfully deleted checked item.");
+          res.redirect("/");
+        }
+      });
+    } else {
+      List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+        if (!err){
+          res.redirect("/" + listName);
+        }
+      });
+    }
+  
+  
+  });
