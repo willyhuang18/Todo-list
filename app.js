31874d0719e64cf94381902eddcd58f0b7b004d4
wiily <willyhuang18@gmail.com>
2022-03-23 21:17:26 -0700

    starting implementing the router for the application

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
