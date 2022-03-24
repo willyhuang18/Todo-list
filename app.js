2ea2ecce3f9f0e8d9a6c8ae2160a3abdab3abfcd
wiily <willyhuang18@gmail.com>
2022-03-23 21:19:17 -0700

    using the mongoose as the database

--- app.js
+++ app.js
@@ -9,3 +9,5 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
