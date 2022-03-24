29129c53dc62198238a84b3fc4e1fda30fbdfa0e
wiily <willyhuang18@gmail.com>
2022-03-23 21:20:09 -0700

    declaring the item model as the seed

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+const itemsSchema = {
+    name: String
+  };
+  
+  const Item = mongoose.model("Item", itemsSchema);
+  
+  
+  const item1 = new Item({
+    name: "Welcome to your todolist!"
+  });
+  
+  const item2 = new Item({
+    name: "Hit the + button to add a new item."
+  });
+  
+  const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+  });
+  
+  const defaultItems = [item1, item2, item3];
